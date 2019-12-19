5a25c4ae280a89d8867062ff5a4aa4640eb7dfc2
Josh Kaderlan <josh@lob.com>
2019-12-19 14:45:38 -0800

    feat(movies): linting

--- db/migrations/20191219134448_add_name_to_movies.js
+++ db/migrations/20191219134448_add_name_to_movies.js
@@ -1,19 +1,19 @@
 'use strict';
 
-exports.up = function(Knex) {
+exports.up = function (Knex) {
   return Knex.schema.table('movies', (table) => {
     table.text('name');
   })
   .then(() => {
     return Knex.raw('ALTER TABLE movies ALTER COLUMN title DROP NOT NULL');
-  });  
+  });
 };
 
-exports.down = function(Knex) {
+exports.down = function (Knex) {
   return Knex.schema.table('movies', (table) => {
     table.dropColumn('name');
   })
   .then(() => {
     return Knex.raw('ALTER TABLE movies ALTER COLUMN title SET NOT NULL');
-  });  
+  });
 };
