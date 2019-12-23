f1e7c2783acb991e8eb5d059031bc933ce261407
Josh Kaderlan <josh@lob.com>
2019-12-23 11:36:03 -0800

    linting

--- db/migrations/20191223112904_drop_title_from_movies.js
+++ db/migrations/20191223112904_drop_title_from_movies.js
@@ -1,13 +1,14 @@
+'use strict';
 
 exports.up = (Knex) => {
   return Knex.schema.table('movies', (table) => {
     table.dropColumn('title');
   })
   .then(() => {
-		return Knex.raw('ALTER TABLE movies ADD CONSTRAINT movies_name_not_null CHECK (name IS NOT NULL) NOT VALID')
-		.then(() => {
-			return Knex.raw('ALTER TABLE movies VALIDATE CONSTRAINT movies_name_not_null');
-		});
+    return Knex.raw('ALTER TABLE movies ADD CONSTRAINT movies_name_not_null CHECK (name IS NOT NULL) NOT VALID')
+    .then(() => {
+      return Knex.raw('ALTER TABLE movies VALIDATE CONSTRAINT movies_name_not_null');
+    });
   });
 };
 
@@ -17,5 +18,5 @@ exports.down = (Knex) => {
   })
   .then(() => {
     return Knex.raw('ALTER TABLE movies DROP CONSTRAINT movies_name_not_null');
-  });  
+  });
 };
